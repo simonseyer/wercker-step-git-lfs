6af1dbff53bbda9fd27012d1180ce06db7405a9b
Simon Seyer <simon.seyer@gmail.com>
2018-06-04 23:39:45 +0200

    Removed useless SSH mitigation

--- run.sh
+++ run.sh
@@ -3,10 +3,5 @@ curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.s
 sudo apt-get install git-lfs
 sudo git lfs install
 
-info 'Fixing SSH issue'
-cat .ssh/known_hosts
-sudo ssh-keygen -R github.com
-cat .ssh/known_hosts
-
 info 'Updating repo with lfs'
 git lfs pull
